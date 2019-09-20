1d82f6caaca2640d4976c694f8bff3e2ea295ee8
Khang Khag <khangkhag@Khag.local>
2019-09-20 13:56:47 +0700

    succesfully get the sprite but need to unified same sprites

--- find_sprites2.py
+++ find_sprites2.py
@@ -21,30 +21,37 @@ def find_sprites(image=None, background_color=None):
     @image: MUST be an Image object
     @background_color: optinal, whether tuple (RGB/ RGBA) or int (grayscale)
     """
-    def is_ignored_pixel(current_pixel, numpy_array):
-        if (background_color == 0 and current_pixel[-1] == 0) or (current_pixel == array(background_color)).all() or (image.mode == "L" and current_pixel == background_color):
-            return True
-        return False
 
     def is_new_sprite(current_row, current_column, pixels_matrix, background_color):
         """Return False if there is a non-background pixel adjacent to current pixel
         Ignores background pixels.
         """
-        neighbor_coordinates = [(-1, 1), (0, 1), (1, 1), (-1, 0)]
+        neighbor_coordinates = [(-1, -1), (-1, 0), (-1, 1), (0, -1)]
         current_pixel = pixels_matrix[current_row][current_column]
+        is_new_sprite = True
 
+        # Ignore background pixels
         if current_pixel.is_background_pixel:
             return False
 
+        # Check 4 neighbor of current pixels
         for coordinate in neighbor_coordinates:
-            neighbor_row = current_row + coordinate[1]
-            neighbor_column = current_column + coordinate[0]
+            neighbor_row = current_row + coordinate[0]
+            neighbor_column = current_column + coordinate[1]
             if 0 <= neighbor_row < image_height and 0 <= neighbor_column < image_width:
                 neighbor_pixel = pixels_matrix[neighbor_row][neighbor_column]
-                if neighbor_pixel.label != 0:
+                if neighbor_pixel.label == 0:
+                    continue
+                
+                else:
                     pixels_matrix[current_row][current_column].label = neighbor_pixel.label
-                    return False
-        return True
+                    is_new_sprite = False
+        return is_new_sprite
+
+    def is_ignored_pixel(current_pixel, numpy_array):
+        if (background_color == 0 and current_pixel[-1] == 0) or (current_pixel == array(background_color)).all() or (image.mode == "L" and current_pixel == background_color):
+            return True
+        return False
 
     def analyze_numpy_array(background_color):
         """Convert image to numpy array then analyze each pixel
@@ -67,7 +74,7 @@ def find_sprites(image=None, background_color=None):
                     pixels_matrix[row_index][column_index].label = new_label
                     sprites_label.append(new_label)
 
-        pprint(pixels_matrix[0:2], width=120)
+        pprint(pixels_matrix, width=120)
 
     def is_valid_background_color():
         """Check if arg background_color is valid
